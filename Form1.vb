a2b7cfeed6d3c0035e435f01612fe18a8f9c2431
Victor Micheni <victormichenim@gmail.com>
2022-11-13 09:31:41 +0300

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,11 @@
+﻿Imports System.Drawing.Text
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim num1, num2, sum As Single
+        num1 = Textbox1.Text
+        num2 = TextBox2.Text
+        sum = num1 + num2
+        label1.Text = sum
+    End Sub
+End Class
